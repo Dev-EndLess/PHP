83846eb0da2d7f229c0658a4af1a073d77464f84
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-04-08 21:28:57 +0200

    exception

---
+++ 15_exception.php
@@ -0,0 +1,28 @@
+<?php
+
+/* ----------- Exceptions ----------- */
+
+/*
+  PHP has an exception model similar to that of other programming languages. An exception can be thrown, and caught ("catched") within PHP. Code may be surrounded in a try block, to facilitate the catching of potential exceptions. Each try must have at least one corresponding catch or finally block.
+*/
+
+function inverse($x) {
+  if (!$x) {
+    throw new Exception('Division by zero.');
+  }
+  return 1/$x;
+}
+
+// echo inverse(0); // Causes an exception to be thrown and stops script execution
+
+// Handles the exception
+try {
+  echo inverse(5) . ' ';
+  echo inverse(0) . ' ';
+} catch (Exception $error) {
+  echo '<br>';
+  echo 'Caught exception: ',  $error->getMessage();
+} finally {
+    echo '<br>';
+    echo '<p style="color: green;">Finally Completed </p>';
+  }
